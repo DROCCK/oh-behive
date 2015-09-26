c6d64b459bfff5399e3d86fbff394e2e475a9a22
ChiefTwoPencils <robertwilk916@gmail.com>
2015-09-26 14:47:40 -0700

    Added a msg to index.html.

--- src/main/java/drocck/sp/beesandhoney/controllers/IndexController.java
+++ src/main/java/drocck/sp/beesandhoney/controllers/IndexController.java
@@ -1,8 +1,17 @@
 package drocck.sp.beesandhoney.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * @author Robert Wilk
  *         Created on 9/26/2015.
  */
+@Controller
 public class IndexController {
+
+    @RequestMapping("/")
+    public String index() {
+        return "index";
+    }
 }
