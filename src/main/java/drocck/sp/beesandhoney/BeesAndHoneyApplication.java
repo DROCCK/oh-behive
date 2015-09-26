40e2f6f4e6c72e53c0c1ea5b2876231cefc96511
ChiefTwoPencils <robertwilk916@gmail.com>
2015-09-26 11:25:41 -0700

    Initial commit

---
+++ src/main/java/drocck/sp/beesandhoney/BeesAndHoneyApplication.java
@@ -0,0 +1,12 @@
+package drocck.sp.beesandhoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeesAndHoneyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeesAndHoneyApplication.class, args);
+    }
+}
---
+++ src/test/java/drocck/sp/beesandhoney/BeesAndHoneyApplicationTests.java
@@ -0,0 +1,18 @@
+package drocck.sp.beesandhoney;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = BeesAndHoneyApplication.class)
+@WebAppConfiguration
+public class BeesAndHoneyApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
