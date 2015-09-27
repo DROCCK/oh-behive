81e0fbedf62a60b815ecd9da2dacf0a85f374139
ChiefTwoPencils <robertwilk916@gmail.com>
2015-09-26 22:41:53 -0700

    Added Apache Tiles support and bean configs.

--- src/main/java/drocck/sp/beesandhoney/web/AppConfig.java
+++ src/main/java/drocck/sp/beesandhoney/web/AppConfig.java
@@ -4,12 +4,58 @@ import drocck.sp.beesandhoney.business.entities.repositories.YardRepository;
 import drocck.sp.beesandhoney.business.services.YardService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
+import org.springframework.web.servlet.ViewResolver;
+import org.thymeleaf.extras.tiles2.dialect.TilesDialect;
+import org.thymeleaf.extras.tiles2.spring4.web.configurer.ThymeleafTilesConfigurer;
+import org.thymeleaf.extras.tiles2.spring4.web.view.ThymeleafTilesView;
+import org.thymeleaf.spring4.SpringTemplateEngine;
+import org.thymeleaf.spring4.view.ThymeleafViewResolver;
 
 /**
- * Created by Connor on 9/26/2015.
+ * @author Robert Wilk
+ * @author Connor Elison
+ * Created on 9/26/2015.
  */
 @Configuration
 public class AppConfig {
+
+    @Bean
+    public ViewResolver tilesViewResolver() {
+        ThymeleafViewResolver vr = new ThymeleafViewResolver();
+        vr.setTemplateEngine(templateEngine());
+        vr.setViewClass(ThymeleafTilesView.class);
+        vr.setCharacterEncoding("UTF-8");
+        vr.setOrder(Ordered.LOWEST_PRECEDENCE);
+        return vr;
+    }
+
+    @Bean
+    public ViewResolver thymeleafViewResolver() {
+        ThymeleafViewResolver vr = new ThymeleafViewResolver();
+        vr.setTemplateEngine(templateEngine());
+        vr.setCharacterEncoding("UTF-8");
+        vr.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        // all message/* views will not be handled by this resolver;
+        vr.setExcludedViewNames(new String[]{"message/*"});
+        return vr;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        //...
+        templateEngine.addDialect(new TilesDialect());
+        return templateEngine;
+    }
+
+    @Bean
+    public ThymeleafTilesConfigurer tilesConfigurer() {
+        ThymeleafTilesConfigurer ttc = new ThymeleafTilesConfigurer();
+        ttc.setDefinitions("/resources/templates/tiles-defs.xml");
+        return ttc;
+    }
+
     @Bean
     public YardService yardService() {
         return new YardService();
