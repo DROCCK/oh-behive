b37174c068a3760ca4cfdb9abb7a1b941363fa20
chaithao <chaithao@csus.edu>
2015-10-17 13:15:55 -0700

    Shipment Class

---
+++ src/main/java/drocck/sp/beesandhoney/business/entities/Shipment.java
@@ -0,0 +1,86 @@
+package drocck.sp.beesandhoney.business.entities;
+
+import javax.persistence.*;
+
+/**
+ * Created by Chai on 10/9/2015.
+ */
+@Entity
+public class Shipment {
+
+    @Id
+    @Column(name = "ID")
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id = null;
+
+    @OneToOne(cascade = CascadeType.ALL)
+    @PrimaryKeyJoinColumn(name = "ID")
+    private Shipment shipment = null;
+
+    @OneToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "TO_YARD")
+    private Yard toYard;
+
+    @OneToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "FROM_YARD")
+    private Yard fromYard;
+
+    @Column(name = "SINGLEHIVE")
+    private String singleHive = null;
+
+    @Column(name = "DOUBLEHIVE")
+    private String doubleHive = null;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setShipment(Shipment shipment) {
+        this.shipment = shipment;
+    }
+
+    public Shipment getShipment() {
+        return shipment;
+    }
+
+    public void setSingleHive(String singleHive) {
+        this.singleHive = singleHive;
+    }
+
+    public String getSingleHive() {
+        return singleHive;
+    }
+
+    public void setDoubleHive(String doubleHive) {
+        this.doubleHive = doubleHive;
+    }
+
+    public String getDoubleHive() {
+        return doubleHive;
+    }
+
+    public void setToYard(Yard toYard) {
+        this.toYard = toYard;
+    }
+
+    public Yard getToYard() {
+        return toYard;
+    }
+
+    public void setFromYard(Yard fromYard) {
+        this.fromYard = fromYard;
+    }
+
+    public Yard getFromYard() {
+        return fromYard;
+    }
+
+    @Override
+    public String toString() {
+        return "Shipment [id="+this.id+" toYard="+this.toYard.getId()+" fromYard ="+this.fromYard.getId() +" singleHive="+this.singleHive +" doubleHive="+this.doubleHive+"]";
+    }
+}
