ef0ea18365a631f56174b292587c9386933a6823
kmatyac <jkm242@csus.edu>
2015-10-31 14:36:34 -0700

    Employee update fixed.

--- src/main/java/drocck/sp/beesandhoney/business/services/EmployeeService.java
+++ src/main/java/drocck/sp/beesandhoney/business/services/EmployeeService.java
@@ -1,6 +1,7 @@
 package drocck.sp.beesandhoney.business.services;
 
 import drocck.sp.beesandhoney.business.entities.Employee;
+import drocck.sp.beesandhoney.business.entities.User;
 import drocck.sp.beesandhoney.business.entities.repositories.EmployeeRepository;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,9 +31,24 @@ public class EmployeeService {
     }
 
     public Employee save(Employee employee) {
-        //personService.save(person);
-
         return employeeRepository.save(employee);
+    }
+
+    public Employee update(Employee employee) {
+
+        Employee e = employeeRepository.findById(employee.getId());
+        //e.getPerson().setName(e.getPerson().getName());
+        e.getPerson().setName(employee.getPerson().getName());
+        e.getUser().setUsername(employee.getUser().getUsername());
+        e.getUser().setPassword(employee.getUser().getPassword());
+        e.getPerson().setContactInfo(employee.getPerson().getContactInfo());
+        e.getPerson().getContactInfo().setId(e.getId());
+        e.getPerson().getContactInfo().setAddress(e.getPerson().getContactInfo().getAddress());
+        e.getPerson().getContactInfo().getAddress().setId(e.getId());
+
+        //personService.update(employee.getPerson());
+        //userService.update(employee.getUser());
+        return employeeRepository.save(e);
     }
 
     public void delete(Long id) { employeeRepository.delete(id); }
