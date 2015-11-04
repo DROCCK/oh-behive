d468ebeaafcf34d44aa2993ebc47d23f45817e09
Connor Elison <cjelison@gmail.com>
2015-11-03 21:00:28 -0800

    onBack() added

--- src/main/resources/static/js/helpers.js
+++ src/main/resources/static/js/helpers.js
@@ -2,3 +2,11 @@
  * @author Robert Wilk
  * Created on 10/5/2015.
  */
+
+function goBack() {
+    if(confirm("Are you sure you want to navigate away from this page?"))
+    {
+        history.go(-1);
+    }
+    return false;
+};
