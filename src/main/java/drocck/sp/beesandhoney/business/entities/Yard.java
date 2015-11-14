7d68f6d3ecddb5a613cf0725a4c958b392ce59bb
chaithao <chaithao@csus.edu>
2015-11-13 17:43:02 -0800

    Added supers and duds to yards.

--- src/main/java/drocck/sp/beesandhoney/business/entities/Yard.java
+++ src/main/java/drocck/sp/beesandhoney/business/entities/Yard.java
@@ -65,8 +65,17 @@ public class Yard implements Serializable {
     @Column(name = "DOUBLES")
     private Integer doubles;
 
+    @Column(name = "SUPERS")
+    private Integer supers;
+    //number of smaller hives that stack up on top of the singles/doubles.
+
+    @Column(name = "DUDS")
+    private Integer duds;
+    //number of hives with dead queen or all bees dead.
+
     public Integer getDoubles() {
         return doubles == null ? 0 : doubles;
+        // return doubles;
     }
 
     public void setDoubles(Integer doubles) {
@@ -75,10 +84,27 @@ public class Yard implements Serializable {
 
     public Integer getSingles() {
         return singles == null ? 0 : singles;
+        // return singles;
     }
 
     public void setSingles(Integer singles) {
         this.singles = singles;
+    }
+
+    public void setSupers(Integer supers) {
+        this.supers = supers;
+    }
+
+    public Integer getSupers() {
+        return supers == null ? 0 : supers;
+    }
+
+    public void setDuds(Integer duds) {
+        this.duds = duds;
+    }
+
+    public Integer getDuds() {
+        return duds == null ? 0 : duds;
     }
 
     public Integer getCurrentHives() {
