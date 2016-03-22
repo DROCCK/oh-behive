37c38468eec3fc351359ed86885cd4d32ac50e93
Connor Elison <cjelison@gmail.com>
2016-03-22 13:47:34 -0700

    Fixed an issue where server tries to create duplicate admin

--- src/main/java/drocck/sp/beesandhoney/business/services/DataInitService.java
+++ src/main/java/drocck/sp/beesandhoney/business/services/DataInitService.java
@@ -4,10 +4,8 @@ import drocck.sp.beesandhoney.business.entities.Role;
 import drocck.sp.beesandhoney.business.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.keygen.StringKeyGenerator;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
 import java.util.Collections;
 
 /**
@@ -25,16 +23,18 @@ public class DataInitService {
 
     public void init() {
 
-        Role role = new Role();
-        role.setName("ADMIN");
-        roleService.save(role);
+        if (!(userService.getUserByUsername("admin@admin.com").isPresent())) {
+            Role role = new Role();
+            role.setName("ADMIN");
+            roleService.save(role);
 
-        User user = new User();
-        user.setUsername("admin@admin.com");
-        String passHash = new BCryptPasswordEncoder().encode("password");
-        System.out.println("PASSWORD BCRYPT HASH = " + passHash);
-        user.setPassword(passHash);
-        user.setRoles(Collections.singletonList(role));
-        userService.save(user);
+            User user = new User();
+            user.setUsername("admin@admin.com");
+            String passHash = new BCryptPasswordEncoder().encode("password");
+            System.out.println("PASSWORD BCRYPT HASH = " + passHash);
+            user.setPassword(passHash);
+            user.setRoles(Collections.singletonList(role));
+            userService.save(user);
+        }
     }
 }
