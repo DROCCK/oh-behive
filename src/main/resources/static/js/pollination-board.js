b5c74b1548341d459edf03efb0c096fb741cf3ea
ChiefTwoPencils <robertwilk916@gmail.com>
2016-04-11 16:09:13 -0700

    Contract details and list have checks instead of trash cans.

--- src/main/resources/static/js/pollination-board.js
+++ src/main/resources/static/js/pollination-board.js
@@ -574,11 +574,11 @@ function loadContractDetails(data) {
     $('#broker').html('<b>Broker: </b>' + (data.broker == null ? '' : data.broker.name));
     $('#number').html('<b>Phone: </b>' + (data.broker == null ? '' : data.broker.contactInfo == null ? '' : data.broker.contactInfo.phone));
     $('#edit').html('<a href="#"><i class="material-icons md-24 bee-board-icon">create</i></a>');
-    $('#delete').html('<a href="#"><i class="material-icons md-24 bee-board-icon">delete</i></a>');
+    $('#delete').html('<a href="#"><i class="material-icons md-24 bee-board-icon">check</i></a>');
     $('#contacts').html('<a href=#><i class="material-icons md-24 bee-board-icon" data-toggle="modal" ' +
         'data-target="#table-modal" onclick="getContacts(0)">person_outline</i></a>');
     $('#shipments').html('<a href="#"><i class="material-icons md-24 bee-board-icon" data-toggle="modal" ' +
-        'data-target="#table-modal" onclick="loadShipmentListModal()">visibility</i></a>');
+        'data-target="#table-modal" onclick="loadShipmListModal()">visibility</i></a>');
     $('#progress').html('<b>% Fulfilled:</b><br/><div class="progress"><div class="progress-bar" role="progressbar" ' +
         'aria-valuemin="0" aria-valuemax="100" aria-valuenow="' + data.count + '" style="width: ' + data.count + '%"></div></div>'
     );
--- src/main/resources/static/js/tables.js
+++ src/main/resources/static/js/tables.js
@@ -18,6 +18,14 @@ function deleteFormatter(value, row, index) {
     ].join('');
 }
 
+function checkFormatter(value, row, index) {
+    return [
+        '<a class="remove ml10 bee-board-icon" href="javascript:void(0)" title="Complete">',
+        ' <i class="material-icons bee-board-icon">check</i>',
+        '</a>'
+    ].join('');
+}
+
 $(function () {
     $('#contract-table').bootstrapTable({}).on('click-row.bs.table', function (e, row, $element) {
         tableRowClick(e, row, $element);
